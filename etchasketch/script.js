c851e4d642ddf5398054c44d75537894db4350be
edlally <eddielally@outlook.com>
2025-08-26 16:32:34 +0100

    Add drawing and color select functionality

--- etchasketch/script.js
+++ etchasketch/script.js
@@ -1,10 +1,12 @@
+const body = document.querySelector("#body")
 const display = document.querySelector("#screen");
-const grid = document.querySelector("#grid");
 const knob1 = document.querySelector("#knob1");
 const knob2 = document.querySelector("#knob2");
-const slider = document.querySelector("#gridSlider")
-
-
+const slider = document.querySelector("#gridSlider");
+const sliderVal = document.querySelector("#sliderRange");
+const clearBtn = document.querySelector("#clear");
+const colorBtn = document.querySelector("#color");
+const colorPicker = document.querySelector("#colorPicker");
 // rotate the knobs based on cursor pos
 display.addEventListener("mousemove", (e) => {
     knob1.style.transform = `rotate(${e.clientX}deg)`;
@@ -12,21 +14,49 @@ display.addEventListener("mousemove", (e) => {
 });
 
 // add grid based on slider value
-slider.addEventListener("click", addGrid())
 
 function addGrid () {
     display.innerHTML = '';
     for (let i=0; i < slider.value ** 2; i++) {
         const box = document.createElement("div");
-        box.style.width = `calc(100% / ${slider.value}`;
-        box.style.height = `calc(100% / ${slider.value}`;
+        box.style.width = `calc(100% / ${slider.value})`;
+        box.style.height = `auto`;
         box.className = `box`;
+        box.addEventListener("mouseover", () => {
+            box.style.backgroundColor = `${colorPicker.value}`;
+            box.style.boxShadow = `10px 10px 5px #e7e7e7ff`;
+        })
         display.append(box);
     };
 };
 
-// add grid based on slider value
-slider.addEventListener("click", addGrid)
+// add initial grid
+addGrid();
+
+// update the grid when the slider is set
+slider.addEventListener("click", addGrid);
+
+// update the slider value text
+slider.oninput = function() {
+    sliderVal.style.color = `rgba(0, 0, 0, 0.5)`;
+    sliderVal.innerHTML = `${this.value}`;
+    setTimeout(() => {
+        sliderVal.style.color = `rgba(0, 0, 0, 0)`;
+    }, 1000);
+};
+
+
+// clear function
+clearBtn.addEventListener("click", addGrid);
+
+
+// color picker 
+colorPicker.click();
+colorBtn.addEventListener("click", () => colorPicker.click());
+
+
+
+
     
 
-// on mousedown change target element to color
+
